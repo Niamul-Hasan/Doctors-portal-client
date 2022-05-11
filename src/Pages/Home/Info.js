2ea2fb5539e6ea159d28179f5f60b920f63e9fe8
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-05-12 01:17:12 +0600

    first commit

---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import InfoCart from './InfoCart';
+import clock from '../../assets/icons/clock.svg';
+
+const Info = () => {
+    return (
+        <div class='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            <InfoCart img={clock}></InfoCart>
+            <InfoCart img={clock}></InfoCart>
+            <InfoCart img={clock}></InfoCart>
+        </div>
+    );
+};
+
+export default Info;
